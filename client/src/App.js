ffbfba275373ed77daa3769070b2a27e0e33781d
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-08-04 15:24:46 -0400

    delete unused files, add bootstrap and change project title

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,7 @@
-
 import './styles/App.css';
-import { Button, AppBar, Toolbar, Box }from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-        <AppBar position="static" color="primary"> 
-          <Toolbar variant="dense" sx={{ justifyContent: "space-between" }}>
-            <Box>
-              <Button variant="contained">Login</Button>
-              <Button variant="contained">Logout</Button>
-            </Box>
-            <Button variant="contained" size="medium" color="warning" >Get Started</Button>
-          </Toolbar>
-          
-        </AppBar>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
