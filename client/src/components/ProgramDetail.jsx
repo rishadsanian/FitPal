ba8856a43f39c7f2863dda9d42fad3803c24ecd4
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-08-04 17:39:44 -0400

    seperate session to SessionItem

--- client/src/components/ProgramDetail.jsx
+++ client/src/components/ProgramDetail.jsx
@@ -1,118 +1,28 @@
+import SessionItem from "./SessionItem";
+
 const ProgramDetail = () => {
   return (
     <div>
       <div class="p-3 m-4 text-start bg-dark">
-      <div class="container-fluid py-5">
-        <h1 class="display-5 fw-bold text-warning">Program name</h1>
-        <p class="col-md-8 fs-4 text-white">Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ab doloremque sint ea, atque velit? Numquam sequi minus dolorem iusto nostrum, doloribus quod eligendi commodi consequatur suscipit, rem porro? Dolore!</p>
+        <div class="container-fluid py-5">
+          <h1 class="display-5 fw-bold text-warning">Program name</h1>
+          <p class="col-md-8 fs-4 text-white">
+            Description: Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Deleniti ab doloremque sint ea, atque velit? Numquam
+            sequi minus dolorem iusto nostrum, doloribus quod eligendi
+            commodi consequatur suscipit, rem porro? Dolore!
+          </p>
+        </div>
       </div>
-    </div>
 
       <div>
-        <h3 className="fw-bold">Session List</h3>
+        <h1 className="fw-bold">Session List</h1>
         <div className="px-4 row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
-          <div className="col my-3">
-            <div className="card">
-              <div className="card-header fw-bold">
-                Chest day
-              </div>
-              <div className="card-body">
-                <p className="card-text text-start">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer d-flex flex-wrap justify-content-between">
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="col my-3">
-            <div className="card">
-              <div className="card-header fw-bold">
-                Chest day
-              </div>
-              <div className="card-body">
-                <p className="card-text text-start">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer d-flex flex-wrap justify-content-between">
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="col my-3">
-            <div className="card">
-              <div className="card-header fw-bold">
-                Chest day
-              </div>
-              <div className="card-body">
-                <p className="card-text text-start">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer d-flex flex-wrap justify-content-between">
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="col my-3">
-            <div className="card">
-              <div className="card-header fw-bold">
-                Chest day
-              </div>
-              <div className="card-body">
-                <p className="card-text text-start">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer d-flex flex-wrap justify-content-between">
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="col my-3">
-            <div className="card">
-              <div className="card-header fw-bold">
-                Chest day
-              </div>
-              <div className="card-body">
-                <p className="card-text text-start">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer d-flex flex-wrap justify-content-between">
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-              </div>
-            </div>
-          </div>
-
-          <div className="col my-3">
-            <div className="card">
-              <div className="card-header fw-bold">
-                Chest day
-              </div>
-              <div className="card-body">
-                <p className="card-text text-start">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer d-flex flex-wrap justify-content-between">
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-                <span class="badge text-bg-secondary my-1">45lsb / 8</span>
-              </div>
-            </div>
-          </div>
-          
+          <SessionItem />
+          <SessionItem />
+          <SessionItem />
+          <SessionItem />
+          <SessionItem />
         </div>
       </div>
     </div>
---
+++ client/src/components/SessionItem.jsx
@@ -0,0 +1,18 @@
+const SessionItem = () => {
+  return (
+    <div className="col my-3">
+      <div className="h-100 p-3 text-bg-dark rounded-3 text-start">
+        <h2 className="text-warning">Chest day</h2>
+        <p className="card-text text-start">
+          Some quick example text to build on the card title and make up
+          the bulk of the card's content.
+        </p>
+        <button class="btn btn-outline-light" type="button">
+          See it
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SessionItem;
