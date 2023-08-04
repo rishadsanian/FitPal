bd7e265e767dcb09be994f17481389236979a864
Rishad Alam <rishad.s.a@gmail.com>
2023-08-04 15:59:59 -0400

    log component add states, add form, add jsx html, add mock data

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-
-import './styles/App.css';
-import { Button, AppBar, Toolbar, Box }from '@mui/material';
-
+import "./styles/App.css";
+import { Button, AppBar, Toolbar, Box } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import Log from "./components/Log";
 function App() {
   return (
     <div className="App">
-        <AppBar position="static" color="primary"> 
-          <Toolbar variant="dense" sx={{ justifyContent: "space-between" }}>
-            <Box>
-              <Button variant="contained">Login</Button>
-              <Button variant="contained">Logout</Button>
-            </Box>
-            <Button variant="contained" size="medium" color="warning" >Get Started</Button>
-          </Toolbar>
-          
-        </AppBar>
+      <AppBar position="static" color="primary">
+        <Toolbar variant="dense" sx={{ justifyContent: "space-between" }}>
+          <Box>
+            <Button variant="contained">Login</Button>
+            <Button variant="contained">Logout</Button>
+          </Box>
+          <Button variant="contained" size="medium" color="warning">
+            Get Started
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+    <Log/>
     </div>
   );
 }
-
 export default App;
