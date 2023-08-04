ad10b9c5d303ed54903e93175fb4ef6f6934ac50
dburnham1212 <dburnham1212@gmail.com>
2023-08-04 17:22:45 -0400

    Finished home page layout

--- client/src/components/LandingCard.jsx
+++ client/src/components/LandingCard.jsx
@@ -4,41 +4,20 @@ import { Box, Card, CardMedia, CardContent, Typography }from '@mui/material';
  function LandingCard(props){
   return (
     <div>
-      <div className="card mt-4 mb-4" style={{ width: "90%", margin: "auto"}}>
+      <div className="card mx-auto mt-4 mb-4" style={{ width: "90%"}}>
         <div className="row g-0">
           <div className="col-md-4">
-            <img src="https://images.pexels.com/photos/2294361/pexels-photo-2294361.jpeg" className="img-fluid rounded-start" alt="..." />
+            <img src={props.image_source} className="img-fluid rounded-start" alt="..." />
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h5 className="card-title">{props.feature}</h5>
-              <p className="card-text">{ props.feature_tag }</p>
-              <p className="card-text">{ props.feature_text }</p>
+              <h5 className="card-title">{props.title}</h5>
+              <p className="card-text">{ props.tagline }</p>
+              <p className="card-text">{ props.description }</p>
             </div>
           </div>
         </div>
       </div>
-      {/* <Card sx={{ display: "flex", justifyContent: "space-between", width: '90%', margin: "auto" }}>
-        <Box sx={{ display: 'flex', flexDirection: 'column'}}>
-          <CardContent>
-            <Typography component="div" variant="h5">
-              props.feature
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary" component="div">
-              { props.feature_tag }
-            </Typography>
-            <Typography variant="body1" color="text.secondary" component="div">
-              { props.feature_text }
-            </Typography>
-          </CardContent>
-        </Box>
-        <CardMedia
-          component="img"
-          sx={{ width: "50%", padding: 2 }}
-          image="https://images.pexels.com/photos/2294361/pexels-photo-2294361.jpeg"
-          alt="Workout Picture"
-        />
-      </Card> */}
     </div>
   )
 }
--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -3,20 +3,53 @@ import { Box }from '@mui/material';
 import HeroImage from "./HeroImage";
 import LandingCard from "./LandingCard";
 
+const featureData = [
+  {
+    id: 0,
+    title: "Workout Tracking",
+    tagline: "tagline",
+    description: "Fit Pal allows users to effortlessly track their sets and reps during daily workout sessions. It provides an intuitive interface to record exercise details, weights used, and the number of repetitions performed. Users can easily refer back to their workout history and monitor their progress over time.",
+    image_source: "https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg"
+  
+  },
+  {
+    id: 1,
+    title: "Customizable Workout Programs",
+    tagline: "tagline",
+    description: "Fit Pal offers a diverse range of workout programs that users can choose from or customize to suit their individual preferences. Users can select the number of workout days per week, the number of exercises per day, and even specify their preferred exercise types, such as strength training, cardio, flexibility, or a combination of these.",
+    image_source: "https://images.pexels.com/photos/1552242/pexels-photo-1552242.jpeg"
+  },
+  {
+    id: 2,
+    title: "Personalized Exercise Recommendations",
+    tagline: "tagline",
+    description: "The app uses advanced algorithms to analyze user attributes, including body weight, fitness level, and workout history. Based on this information, Fit Pal will provide personalized exercise recommendations tailored to individual goals, such as muscle building, weight loss, or overall fitness improvement.",
+    image_source: "https://images.pexels.com/photos/1954524/pexels-photo-1954524.jpeg"
+  },
+  {
+    id: 3,
+    title: "Analytics and Progress Visualization",
+    tagline: "tagline",
+    description: "Fit Pal presents users with detailed analytics and visualizations of their fitness journey. Users can track their performance trends, improvements, and areas for potential growth. The app will showcase data in charts and graphs, making it easier for users to interpret their progress.",
+    image_source: "https://images.pexels.com/photos/4761663/pexels-photo-4761663.jpeg?"  
+  }
+]
+
+
 export default function LandingPage(){
+  const features = featureData.map(data =>
+    <LandingCard 
+      key={data.id} 
+      title={data.title} 
+      tagline={data.tagline} 
+      description={data.description}
+      image_source={data.image_source}
+    />);
+
   return (
     <div>
       <HeroImage />
-      <LandingCard 
-        feature={"Feature 1"}
-        feature_tag={"Feature Tagline"}
-        feature_text={"Feature text"}
-      />
-      <LandingCard 
-        feature={"Workout Tracking"}
-        feature_tag={"Feature Tagline"}
-        feature_text={"Fit Pal allows users to effortlessly track their sets and reps during daily workout sessions. It provides an intuitive interface to record exercise details, weights used, and the number of repetitions performed. Users can easily refer back to their workout history and monitor their progress over time."}
-      />
+      {features}
     </div>
   )
 }
