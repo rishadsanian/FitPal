233d77a33683aee614990cbd2df79dcd026723be
dburnham1212 <dburnham1212@gmail.com>
2023-08-04 14:20:09 -0400

    Learning how to setup navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Button, AppBar, Toolbar, Box }from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <h1>Fit Pal</h1>
+        <AppBar position="static" color="primary"> 
+          <Toolbar variant="dense" sx={{ justifyContent: "space-between" }}>
+            <Box>
+              <Button variant="contained">Login</Button>
+              <Button variant="contained">Logout</Button>
+            </Box>
+            <Button variant="contained" size="medium" color="warning" >Get Started</Button>
+          </Toolbar>
+          
+        </AppBar>
     </div>
   );
 }
