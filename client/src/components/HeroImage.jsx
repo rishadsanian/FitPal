6947e20383af3307c63a38b2d8ffb69850af99ed
dburnham1212 <dburnham1212@gmail.com>
2023-08-04 16:49:26 -0400

    Stashing files for to pull

--- client/src/components/HeroImage.jsx
+++ client/src/components/HeroImage.jsx
@@ -1,19 +1,9 @@
 import React from "react";
-import { Box }from '@mui/material';
 
  function HeroImage(){
   return (
     <div>
-      <Box
-        component="img"
-        sx={{
-          objectFit: 'cover',
-          height: 400,
-          width: "100%",
-        }}
-        alt="Hero Image"
-        src="https://images.pexels.com/photos/2294361/pexels-photo-2294361.jpeg"
-      />
+      <img className="object-fit-cover" height="400" width="100%" objectFit="cover" src="https://images.pexels.com/photos/2294361/pexels-photo-2294361.jpeg"/>
     </div>
   )
 }
--- client/src/components/LandingCard.jsx
+++ client/src/components/LandingCard.jsx
@@ -4,11 +4,25 @@ import { Box, Card, CardMedia, CardContent, Typography }from '@mui/material';
  function LandingCard(props){
   return (
     <div>
-      <Card sx={{ display: "flex", justifyContent: "space-between", width: '90%', margin: "auto" }}>
+      <div className="card mt-4 mb-4" style={{ width: "90%", margin: "auto"}}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src="https://images.pexels.com/photos/2294361/pexels-photo-2294361.jpeg" className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{props.feature}</h5>
+              <p className="card-text">{ props.feature_tag }</p>
+              <p className="card-text">{ props.feature_text }</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <Card sx={{ display: "flex", justifyContent: "space-between", width: '90%', margin: "auto" }}>
         <Box sx={{ display: 'flex', flexDirection: 'column'}}>
           <CardContent>
             <Typography component="div" variant="h5">
-              { props.feature }
+              props.feature
             </Typography>
             <Typography variant="subtitle1" color="text.secondary" component="div">
               { props.feature_tag }
@@ -24,7 +38,7 @@ import { Box, Card, CardMedia, CardContent, Typography }from '@mui/material';
           image="https://images.pexels.com/photos/2294361/pexels-photo-2294361.jpeg"
           alt="Workout Picture"
         />
-      </Card>
+      </Card> */}
     </div>
   )
 }
