919c9d90f50970f10692f601a98149d7a85337dc
Rishad Alam <rishad.s.a@gmail.com>
2023-08-04 14:13:06 -0400

    eslint format

--- server/routes/catsRoutes.js
+++ server/routes/catsRoutes.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
-const users = require('../db/queries/users');
+const router = require("express").Router();
+const users = require("../db/queries/users");
 
-router.get('/', (req, res) => {
-  users.getAllUsers().then(data => {
+router.get("/", (req, res) => {
+  users.getAllUsers().then((data) => {
     console.log(data);
-    res.json({users: data});
-  })
+    res.json({ users: data });
+  });
 });
-  
+
 module.exports = router;
