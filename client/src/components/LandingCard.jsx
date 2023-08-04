25e9bd883fcc5e55d995c4dd31aaf04ad893e4b2
dburnham1212 <dburnham1212@gmail.com>
2023-08-04 18:00:19 -0400

    Stashing changes to merge

--- client/src/components/LandingCard.jsx
+++ client/src/components/LandingCard.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { Box, Card, CardMedia, CardContent, Typography }from '@mui/material';
 
  function LandingCard(props){
+  const cardBodyClass = props.id % 2 ? "row flex-row-reverse g-0" : "row reverse g-0"
+  const roundedImageClass = props.id % 2 ? "img-fluid rounded-start" : "img-fluid rounded-start";
   return (
     <div>
       <div className="card mx-auto mt-4 mb-4" style={{ width: "90%"}}>
-        <div className="row g-0">
+        <div className={cardBodyClass}>
           <div className="col-md-4">
-            <img src={props.image_source} className="img-fluid rounded-start" alt="..." />
+            <img src={props.image_source} className={roundedImageClass} style={{minHeight: "100%"}}alt="..." />
           </div>
-          <div className="col-md-8">
-            <div className="card-body">
+          <div className="bg-dark text-white col-md-8">
+            <header className ="card-header"> 
               <h5 className="card-title">{props.title}</h5>
+            </header>
+            <div className="card-body">
+
               <p className="card-text">{ props.tagline }</p>
               <p className="card-text">{ props.description }</p>
             </div>
