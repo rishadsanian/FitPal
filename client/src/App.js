5d798f91253dd6f03915e4d873802ad4609578b1
dburnham1212 <dburnham1212@gmail.com>
2023-08-04 16:22:55 -0400

    Working on landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import './styles/App.css';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
+import { AppBar, Toolbar}from '@mui/material';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Login />
+      <LandingPage />
     </div>
   );
 }
