1ea535a30ff1fc78232ce89d10c9eedf851c1504
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-08-04 12:38:19 -0400

    intial setup for server and client

---
+++ server/db/seeds/01_users.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO users (id, email, password) VALUES (1, 'luongtanphat25@gmail.com', 'phat');
+INSERT INTO users (id, email, password) VALUES (2, 'dburnham1212@gmail.com', 'dylan');
+INSERT INTO users (id, email, password) VALUES (3, 'rishad.s.a@gmail.com', 'rishad');;
---
+++ server/db/seeds/02_urls.sql
@@ -0,0 +1,17 @@
+
+-- Seed Phat
+INSERT INTO urls (id, user_id, long_url, short_url, favorite) VALUES (1, 1, 'www.google.ca', 'XcD3d', false);
+INSERT INTO urls (id, user_id, long_url, short_url, favorite) VALUES (2, 1, 'www.intergalacticebay.gl', '3fGvG', true);
+INSERT INTO urls (id, user_id, long_url, short_url, favorite) VALUES (3, 1, 'www.plumbus.gl', 'AAfg6', true);
+INSERT INTO urls (id, user_id, long_url, short_url, favorite) VALUES (4, 1, 'www.fedaration.com', 'AcGj4', false);
+
+-- Seed Dylan
+INSERT INTO urls (id, user_id, long_url, short_url, favorite) VALUES (5, 2, 'www.torontopubliclibrary.ca', 'Jc653', true);
+INSERT INTO urls (id, user_id, long_url, short_url, favorite) VALUES (6, 2, 'www.ontario.ca', 'WaSD4', false);
+INSERT INTO urls (id, user_id, long_url, short_url, favorite) VALUES (7, 2, 'www.apple.ca', '4x0Df', false);
+INSERT INTO urls (id, user_id, long_url, short_url, favorite) VALUES (8, 2, 'www.itchyandscratchshow.com', 'BbCv7', false);
+
+-- Seed Risha
+INSERT INTO urls (id, user_id, long_url, short_url, favorite) VALUES (9, 3, 'www.google.ca', 'iX0ff', false);
+INSERT INTO urls (id, user_id, long_url, short_url, favorite) VALUES (10, 3, 'www.reddit.com', 'KJfCx', false);
+INSERT INTO urls (id, user_id, long_url, short_url, favorite) VALUES (11, 3, 'www.esl-languages.com', 'DxFgg', false);
