b411a5e4f8d369250304b6c50902ac867cfd75ef
Rishad Alam <rishad.s.a@gmail.com>
2023-08-04 22:05:48 -0400

    add schema for profile

---
+++ client/src/hooks/useApplicationData.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import axios from "axios";
+
+const useApplicationData = () => {
+
+
+  //-----------------------------------LOG------------------------------------//
+
+  // // Function to log a workout
+  // const logWorkout = async (data) => {
+  //   try {
+  //     const response = await axios.post("/log", data);
+  //     return response.data; // 
+  //   } catch (error) {
+  //     console.error("Error logging workout:", error);
+  //     return null; //
+  //   }
+  // };
+
+  // // Function to get workout log for a specific user
+  // const getWorkoutLog = async (userId) => {
+  //   try {
+  //     const response = await axios.get(`/log/${userId}`);
+  //     return response.data; //
+  //   } catch (error) {
+  //     console.error("Error getting workout log:", error);
+  //     return null; // Return null or handle the error in some way
+  //   }
+  // };
+
+//---------------------------------------------------------------------------//
+};
+
+export default useApplicationData;
