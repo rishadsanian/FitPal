9b2bc666d18adc4a356581bba7617160dedd4cfa
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-08-05 13:01:30 -0400

    set up routes for programs

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 //routes import
 const catsRoutes = require('./routes/catsRoutes');
+const progamsRoutes = require('./routes/programsRoutes');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(morgan(ENVIROMENT));
 app.use(bodyParser.json());
 
 app.use('/cats', catsRoutes);
+app.use('/api/programs', progamsRoutes);
 
 app.get('/', (req, res) => {
   res.json({ greetings: 'hello world' });
--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -1,15 +1,17 @@
 const db = require('../../configs/db.config');
 
 const getAllUsers = () => {
-	return db.query("SELECT * FROM users;").then(data => {
-		return data.rows;
-	})
-}
+  return db.query('SELECT * FROM users;').then((data) => {
+    return data.rows;
+  });
+};
 
-const getUserById = id => {
-	return db.query("SELECT * FROM users; WHERE id = $1", [id]).then(data => {
-		return data.rows;
-	})
-}
+const getUserById = (id) => {
+  return db
+    .query('SELECT * FROM users; WHERE id = $1', [id])
+    .then((data) => {
+      return data.rows;
+    });
+};
 
-module.exports = {getAllUsers, getUserById}
+module.exports = { getAllUsers, getUserById };
