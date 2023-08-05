5d0707fdc2f42aba2758b09ac901b73b7ac8a5ad
dburnham1212 <dburnham1212@gmail.com>
2023-08-05 16:37:28 -0400

    Stashing changes

---
+++ client/src/mocks/sessionData.js
@@ -0,0 +1,58 @@
+const sessions = [
+  {
+    id: 1,
+    program_id: 1,
+    name: "Compound Power Lifts",
+    description: "This full body strength workout focuses on compound movements like squats, deadlifts, and bench presses to target major muscle groups and build overall strength.",
+  },
+  {
+    id: 2,
+    program_id: 1,
+    name: "Bodyweight Blast",
+    description: "In this session, you'll perform a variety of bodyweight exercises such as push-ups, lunges, and planks to improve strength and build muscular endurance.",
+  },
+  {
+    id: 3,
+    program_id: 1,
+    name: "Resistance Band Rumble",
+    description: "The third workout includes a mix of free-weight and resistance band exercises to challenge your strength and stability, incorporating exercises like bicep curls, rows, and lateral raises.",
+  },
+  {
+    id: 4,
+    program_id: 1,
+    name: "Full Body Circuit",
+    description: "Get ready for a challenging session with this high-intensity full body strength circuit workout, including kettlebell swings, box jumps, and medicine ball slams.",
+  },
+  {
+    id: 5,
+    program_id: 1,
+    name: "Functional Strength Fusion",
+    description: "This workout incorporates functional training movements using kettlebells and dumbbells for overall strength development, combining movements like Turkish get-ups, farmer's carries, and goblet squats.",
+  },
+  {
+    id: 6,
+    program_id: 2,
+    name: "Bench Press Mastery",
+    description: "Focus on building a powerful and well-defined chest with various bench press variations and accessory exercises.",
+  },
+  {
+    id: 7,
+    program_id: 2,
+    name: "Back and Biceps Blast",
+    description: "This session targets the back and biceps muscles with exercises like pull-ups, rows, and bicep curls for a strong and sculpted upper body.",
+  },
+  {
+    id: 8,
+    program_id: 2,
+    name: "Shoulder Strength Builder",
+    description: "Strengthen your shoulders and improve posture with a mix of overhead presses, lateral raises, and rear delt flyes.",
+  },
+  {
+    id: 9,
+    program_id: 2,
+    name: "Arm Pump Intensity",
+    description: "Isolate and pump up those arm muscles with a focus on triceps and biceps exercises to create impressive arm definition.",
+  },
+];
+
+export default sessions;
