7ac7c59197927935e248809d52bd69df86a1b663
Rishad Alam <rishad.s.a@gmail.com>
2023-08-04 23:13:19 -0400

    add profile route on server app.js

--- server/app.js
+++ server/app.js
@@ -48,6 +48,32 @@ app.post('/log', async (req, res) => {
     res.status(500).json({ error: 'Error inserting data' });
   }
 });
-//-------------------------------------------------------------------------
+//---------------------------Profile------------------------------------------//
+
+app.post('/profile', async (req, res) => {
+  try {
+    const { user_id, age, height, weight, gender } = req.body;
+
+    // queryString
+    const queryString = `
+      INSERT INTO Profile (user_id, age, height, weight, gender)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;
+    `;
+
+    // SQL to db
+    const result = await pool.query(queryString, [user_id, age, height, weight, gender]);
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error('Error inserting profile data:', error);
+    res.status(500).json({ error: 'Error inserting profile data' });
+  }
+});
+
+//---------------------------------------------------------------------------//
+
+
+
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
