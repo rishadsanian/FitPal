5131795d1414009f6882d8a29e32e4a132e3e3a3
dburnham1212 <dburnham1212@gmail.com>
2023-08-04 21:57:34 -0400

    Stashing changes

--- client/src/components/DevTest.jsx
+++ client/src/components/DevTest.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import TitleImage from "./TitleImage";
+import HistoryList from "./HistoryList";
 
  function DevTest(){
   return (
     <div>
       <TitleImage />
+      <HistoryList />
     </div>
   )
 }
