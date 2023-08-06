1cf5a4e3ac1a018aad4ce53cf4b6b9f1f81eb353
dburnham1212 <dburnham1212@gmail.com>
2023-08-05 22:23:50 -0400

    Stashing changes

--- client/src/components/HistoryListItem.jsx
+++ client/src/components/HistoryListItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
  function HistoryListItem(props){
   return (
-    <li className="list-group-item bg-dark">
+    <li className="list-group-item bg-dark border-secondary">
       <div className="d-flex justify-content-between align-items-center">
         <span className="text-light">Yesterday</span>
         <button className="btn btn-dark">
