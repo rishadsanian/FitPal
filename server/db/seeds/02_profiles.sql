e59aa1153f5485c0f069e2f479f900f3564ffaef
Rishad Alam <rishad.s.a@gmail.com>
2023-08-06 01:58:50 -0400

    update profile seeds to reflect date of birth instead of age

--- server/db/seeds/02_profiles.sql
+++ server/db/seeds/02_profiles.sql
@@ -1,77 +1,69 @@
+-- Profile data for User 3 (in years)
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('3', '1995-08-06', 170.2, 150.96, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '3 hours');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (3, 28, 170.2, 150.96, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '3 hours');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('3', '1995-07-20', 170.2, 148.81, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '2 weeks' - INTERVAL '3 hours');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (3, 28, 170.2, 148.81, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '2 weeks' - INTERVAL '3 hours');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('3', '1995-07-13', 170.2, 147.24, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '4 weeks' - INTERVAL '3 hours');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (3, 28, 170.2, 147.24, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '4 weeks' - INTERVAL '3 hours');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('3', '1995-06-29', 170.2, 144.65, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '6 weeks' - INTERVAL '3 hours');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (3, 28, 170.2, 144.65, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '6 weeks' - INTERVAL '3 hours');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('3', '1995-06-15', 170.2, 142.99, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '8 weeks' - INTERVAL '3 hours');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (3, 28, 170.2, 142.99, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '8 weeks' - INTERVAL '3 hours');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('3', '1995-05-31', 170.2, 142.20, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '10 weeks' - INTERVAL '3 hours');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (3, 28, 170.2, 142.20, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '10 weeks' - INTERVAL '3 hours');
-
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (3, 28, 170.2, 141.45, 'Male', NOW());
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('3', '1995-05-17', 170.2, 141.45, 'Male', NOW());
 
 -- Weight Progress for User 1 (in pounds)
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (1, 30, 180.5, 165.35, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '2 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('1', '1993-08-06', 180.5, 165.35, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '2 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (1, 30, 180.5, 163.91, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '4 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('1', '1993-07-23', 180.5, 163.91, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '4 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (1, 30, 180.5, 162.18, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '6 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('1', '1993-07-09', 180.5, 162.18, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '6 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (1, 30, 180.5, 160.45, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '8 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('1', '1993-06-25', 180.5, 160.45, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '8 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (1, 30, 180.5, 159.12, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '10 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('1', '1993-06-11', 180.5, 159.12, 'Male', NOW() - INTERVAL '1 month' - INTERVAL '10 weeks');
 
 -- Weight Progress for User 2 (in pounds)
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (2, 25, 165.3, 134.06, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '2 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('2', '1998-08-06', 165.3, 134.06, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '2 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (2, 25, 165.3, 133.10, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '4 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('2', '1998-07-23', 165.3, 133.10, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '4 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (2, 25, 165.3, 132.15, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '6 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('2', '1998-07-09', 165.3, 132.15, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '6 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (2, 25, 165.3, 130.45, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '8 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('2', '1998-06-25', 165.3, 130.45, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '8 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (2, 25, 165.3, 129.28, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '10 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('2', '1998-06-11', 165.3, 129.28, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '10 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (4, 31, 160.8, 121.78, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '2 weeks');
+-- Weight Progress for User 4 (in pounds)
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('4', '1992-08-06', 160.8, 121.78, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '2 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (4, 31, 160.8, 120.60, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '4 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('4', '1992-07-23', 160.8, 120.60, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '4 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (4, 31, 160.8, 119.15, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '6 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('4', '1992-07-09', 160.8, 119.15, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '6 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (4, 31, 160.8, 117.90, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '8 weeks');
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('4', '1992-06-25', 160.8, 117.90, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '8 weeks');
 
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (4, 31, 160.8, 116.80, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '10 weeks');
-
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (1, 30, 180.5, 159.12, 'Male', NOW());
-
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (2, 25, 165.3, 129.28, 'Female', NOW());
-
-INSERT INTO Profile (user_id, age, height, weight, gender, timestamp)
-VALUES (4, 31, 160.8, 116.80, 'Female', NOW());
+INSERT INTO Profile (user_id, date_of_birth, height, weight, gender, timestamp)
+VALUES ('4', '1992-06-11', 160.8, 116.80, 'Female', NOW() - INTERVAL '1 month' - INTERVAL '10 weeks');
