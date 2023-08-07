e798d3aa5bc123ec7b1d124dffda8f4289def3ba
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-08-06 23:36:26 -0400

    add edit and delete button for program card

--- client/src/components/CardListItem.jsx
+++ client/src/components/CardListItem.jsx
@@ -1,26 +1,31 @@
-import React from "react";
+import React from 'react';
 
-
- function ProgramListItem(props){
+function ProgramListItem(props) {
   return (
-    
-      <div className="col my-3">
-        
-          <div className="h-100 p-3 text-bg-dark rounded-3 text-start"> 
-            <div className="h-100 p-3">
-              <h3 className="text-warning">{props.name}</h3>
-                <p>
-                  {props.description}
-                </p>
-                <a href={props.path + props.path_id} class="btn btn-outline-light" type="button">
-                  See it
-                </a>
-            </div>
-            
+    <div className="col my-3">
+      <div className="p-3 text-bg-dark rounded-3 text-start">
+        <div className="h-100 p-3">
+          <h3 className="text-warning">{props.name}</h3>
+          <p>{props.description}</p>
+          <a
+            href={props.path + props.path_id}
+            class="btn btn-outline-light"
+            type="button"
+          >
+            See it
+          </a>
+          <div className="d-flex justify-content-end gap-3">
+            <button className="btn btn-dark">
+              <i class="fa-regular fa-pen-to-square fa-xl text-info"></i>
+            </button>
+            <button className="btn btn-dark">
+              <i class="fa-regular fa-trash-can fa-xl text-danger"></i>
+            </button>
           </div>
-       
-      </div>    
-  )
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProgramListItem;
