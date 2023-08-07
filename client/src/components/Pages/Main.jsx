aef9ab3b5cba218252b3a341819e666540c86202
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-08-06 22:26:47 -0400

    fixed layout of welcome and profile in dashboard

--- client/src/components/Pages/Main.jsx
+++ client/src/components/Pages/Main.jsx
@@ -1,16 +1,14 @@
-import React from "react";
-import { Box, Slide } from "@mui/material";
-import moment from "moment";
-import SliderComponent from "../Slider";
-import Profile from "../Profile";
-import HistoryList from "../HistoryList";
-import Log from "../Log";
-import ChartWorkout from "../ChartWorkout";
-import TitleImage from "../TitleCards/TitleImage";
-
+import React from 'react';
+import { Box, Slide } from '@mui/material';
+import moment from 'moment';
+import SliderComponent from '../Slider';
+import Profile from '../Profile';
+import HistoryList from '../HistoryList';
+import Log from '../Log';
+import ChartWorkout from '../ChartWorkout';
+import TitleImage from '../TitleCards/TitleImage';
 
 //Dashboard/logged in main page
-
 
 const Main = () => {
   //formated date for display
@@ -18,21 +16,26 @@ const Main = () => {
 
   return (
     <div className="bg">
-      <TitleImage icon={"fa-solid fa-table-columns"}/>
+      <TitleImage icon={'fa-solid fa-table-columns'} />
 
-      {/* Header */}
-      <div className="p-3 text-start bg-dark">
-        <div className="container-fluid py-5">
-          <h1 className="display-5 fw-bold text-warning">Welcome User</h1>
-          <p className="col-md-8 fs-4 text-white">{currentDate}</p>
-          <p className="col-md-8 fs-4 text-white">CHAT GPT generated motivation</p>
+      <div className="row row-cols-1 row-cols-md-2">
+        {/* Header */}
+        <div className="p-3 text-start bg-dark col col-md-12 col-lg-4">
+          <div className="container-fluid py-5">
+            <h1 className="display-5 fw-bold text-warning">
+              Welcome User
+            </h1>
+            <p className="col-md-8 text-white">{currentDate}</p>
+            <p className="col-md-8 fs-4 text-white">
+              CHAT GPT generated motivation
+            </p>
+          </div>
         </div>
-      </div>
 
-      {/* Profile Section */}
-      <div className="bg-dark opacity-75">
-        {/* <h3 className="display-5 fw-bold text-warning">Profile</h3> */}
-        <Profile/>
+        {/* Profile Section */}
+        <div className="col col-md-12 col-lg-8">
+          <Profile />
+        </div>
       </div>
 
       {/* Add Workout Section */}
@@ -41,27 +44,27 @@ const Main = () => {
       </div>
 
       {/* Program Schedule Section USE SLIDER OR CAROUSEL*/}
-      <div className="bg-dark p-5">
-        <h1 className="display-5 fw-light text-warning">Program Schedule</h1>
-        <SliderComponent/>
+      <div className="bg-dark opacity-75 p-5">
+        <h1 className="display-5 fw-light text-warning">
+          Program Schedule
+        </h1>
+        <SliderComponent />
       </div>
 
       {/* Recent Activity History Section */}
       <div className="container mt-5">
         {/* <h3 className="display-5 fw-bold text-warning">Recent Activity History</h3> */}
-        <HistoryList/>
+        <HistoryList />
       </div>
 
-        {/* Analytics Section */}
-        <div className="container mt-5">
+      {/* Analytics Section */}
+      <div className="container mt-5">
         {/* <h3 className="display-5 fw-bold text-warning">Analytics</h3> */}
         {/* Exercise Tracker */}
-        <ChartWorkout/>
+        <ChartWorkout />
       </div>
     </div>
   );
 };
-
-
 
 export default Main;
