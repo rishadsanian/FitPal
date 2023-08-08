851da904723862be971d44844c31e1451e0a4a16
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-08-08 11:44:41 -0400

    change edit icon in program edit mode

--- client/src/components/CardListItem.jsx
+++ client/src/components/CardListItem.jsx
@@ -6,14 +6,14 @@ import DeletePopupModal from './DeletePopupModal';
 
 function ProgramListItem(props) {
   const [sessions, setSessions] = useState([]);
-  const [newSessionName, setNewSessionName] = useState("");
+  const [newSessionName, setNewSessionName] = useState('');
   const [displayDeleteModal, setDisplayDeleteModal] = useState(false);
   // State for when editing a program
   const [programUpdate, setProgramUpdate] = useState({
     name: props.name,
-    description: props.description
-  })
-  const [editMode, setEditMode] = useState(false)
+    description: props.description,
+  });
+  const [editMode, setEditMode] = useState(false);
 
   const navigate = useNavigate();
 
@@ -26,25 +26,25 @@ function ProgramListItem(props) {
       });
   }, []);
 
-  
-
   const createNewSession = async (event, program_id) => {
     // prevent the default form action
     event.preventDefault();
     // check if the session name field is not blank
-    if(newSessionName) {
+    if (newSessionName) {
       try {
-        
         // Submit form data to the server
-        const response = await axios.post(`/sessions/program/${program_id}`, {
-          name: newSessionName,
-          program_id
-        });
+        const response = await axios.post(
+          `/sessions/program/${program_id}`,
+          {
+            name: newSessionName,
+            program_id,
+          }
+        );
         // reload the page after the session is created
         window.location.reload();
         // Update the profile state with the newly created/updated profile data
       } catch (error) {
-        console.error("Error creating session:", error);
+        console.error('Error creating session:', error);
       }
     }
   };
@@ -54,70 +54,82 @@ function ProgramListItem(props) {
       // Submit form data to the server
       await axios.post(`/programs/${programId}/delete`);
     } catch (error) {
-      console.error("Error deleting program:", error);
+      console.error('Error deleting program:', error);
     }
-  }
-  
+  };
+
   // navigation for the session
   const navigateToSession = (session) => {
     navigate(`/programs/${props.programId}/sessions/${session.id}`);
-  }
-  
+  };
+
   // function to toggle edit mode
   const toggleEditMode = async (programId) => {
-    if(editMode){
+    if (editMode) {
       try {
         // Submit form data to the server
-        const response = await axios.post(`/programs/${programId}/update`, programUpdate);
+        const response = await axios.post(
+          `/programs/${programId}/update`,
+          programUpdate
+        );
         // reload the page after the session is created
         window.location.reload();
         // Update the profile state with the newly created/updated profile data
       } catch (error) {
-        console.error("Error creating session:", error);
+        console.error('Error creating session:', error);
       }
     } else {
       setEditMode(true);
     }
-  }
+  };
 
   const sessionsListItem = sessions.map((session, index) => {
     return (
       <tr key={index}>
-          <td role="button" onClick={() => navigateToSession(session)}>
-            {session.name}
-          </td>
+        <td role="button" onClick={() => navigateToSession(session)}>
+          {session.name}
+        </td>
       </tr>
     );
   });
-
-  
 
   return (
     <div className="col my-3">
       <div className="card bg-dark text-start">
         {/* Program info */}
-        {editMode ? 
-        <div className="card-body ">
-          {/* If we are in edit mode */}
-          <input 
-            type="text"
-            value={programUpdate.name}
-            className="form-control bg-secondary opacity-75 text-white mb-2"
-            onChange={(e) => setProgramUpdate({...programUpdate, name: e.target.value})}
-          />
-          <textarea
-            type="text"
-            value={programUpdate.description}
-            className="form-control bg-secondary opacity-75 text-white"
-            onChange={(e) => setProgramUpdate({...programUpdate, description: e.target.value})}
-          />
-        </div> 
-        : 
-        <div className="card-body ">
-          {/* If not in edit mode */}
-          <h3 className="text-warning">{props.name}</h3>
-          <p className="text-white">{props.description}</p>
-        </div>}
+        {editMode ? (
+          <div className="card-body ">
+            {/* If we are in edit mode */}
+            <input
+              type="text"
+              value={programUpdate.name}
+              className="form-control bg-secondary opacity-75 text-white mb-2"
+              onChange={(e) =>
+                setProgramUpdate({
+                  ...programUpdate,
+                  name: e.target.value,
+                })
+              }
+            />
+            <textarea
+              type="text"
+              value={programUpdate.description}
+              className="form-control bg-secondary opacity-75 text-white"
+              onChange={(e) =>
+                setProgramUpdate({
+                  ...programUpdate,
+                  description: e.target.value,
+                })
+              }
+            />
+          </div>
+        ) : (
+          <div className="card-body ">
+            {/* If not in edit mode */}
+            <h3 className="text-warning">{props.name}</h3>
+            <p className="text-white">{props.description}</p>
+          </div>
+        )}
         {/* Add session form */}
         <div className="card-body border-top border-color-white">
           <form>
@@ -147,26 +159,41 @@ function ProgramListItem(props) {
 
         {/* Edit - Delete */}
         <div className="d-flex justify-content-end gap-3 p-2 border-top border-color-white">
-          {editMode && <button className="btn btn-dark" onClick={() => setEditMode(false)}>
-            <i className="fa-regular fa-x text-danger fa-xl"></i>
-          </button>}
-          <button className="btn btn-dark" onClick={() => toggleEditMode(props.programId)}>
-            <i className="fa-regular fa-pen-to-square fa-xl text-light"></i>
+          {editMode && (
+            <button
+              className="btn btn-dark"
+              onClick={() => setEditMode(false)}
+            >
+              <i className="fa-regular fa-x text-danger fa-xl"></i>
+            </button>
+          )}
+          <button
+            className="btn btn-dark"
+            onClick={() => toggleEditMode(props.programId)}
+          >
+            {editMode ? (
+              <i class="fa-solid fa-check fa-xl text-warning"></i>
+            ) : (
+              <i className="fa-regular fa-pen-to-square fa-xl text-light"></i>
+            )}
           </button>
-          <button className="btn btn-dark" onClick={() => setDisplayDeleteModal(true)}>
+          <button
+            className="btn btn-dark"
+            onClick={() => setDisplayDeleteModal(true)}
+          >
             <i className="fa-regular fa-trash-can fa-xl text-danger"></i>
           </button>
-          
         </div>
       </div>
       {/* Delete Modal */}
-      {displayDeleteModal && 
-      <DeletePopupModal 
-        modalToggle={setDisplayDeleteModal} 
-        modalAction={deleteProgram}
-        modalParams={props.programId}
-        message={`Deleting program ${props.name}`}
-      />}
+      {displayDeleteModal && (
+        <DeletePopupModal
+          modalToggle={setDisplayDeleteModal}
+          modalAction={deleteProgram}
+          modalParams={props.programId}
+          message={`Deleting program ${props.name}`}
+        />
+      )}
     </div>
   );
 }
