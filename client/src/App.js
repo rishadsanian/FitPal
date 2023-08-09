7b31b416695c1d0177096a3443034bf3d64fb2e2
Rishad Alam <rishad.s.a@gmail.com>
2023-08-09 12:40:33 -0400

    create global user provider to wrap around app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,10 @@ import { useState } from "react";
 import Navbar from "./components/Navbar-Footer/Navbar";
 
 import Main from "./components/Pages/Main";
-import Login from './components/Login-SignUp/Login';
-import LandingPage from './components/Pages/LandingPage';
-import Log from './components/Pages/Log';
-import DevTest from './components/Pages/DevTest';
-
+import Login from "./components/Login-SignUp/Login";
+import LandingPage from "./components/Pages/LandingPage";
+import Log from "./components/Pages/Log";
+import DevTest from "./components/Pages/DevTest";
 
 import SessionDetail from "./components/Sessions/SessionDetail";
 import ExerciseLog from "./components/Exercises/ExerciseLog";
@@ -18,7 +17,8 @@ import Profile from "./components/Dashboard/Profile";
 import ProgramsPage from "./components/Pages/ProgramsPage";
 import ChartWorkout from "./components/Dashboard/ChartWorkout";
 
-import ProgramProvider from './providers/ProgramProvider';
+import ProgramProvider from "./contexts/ProgramProvider";
+import UserProvider from "./contexts/UserContext";
 
 function App() {
   const [authenticated, setAuthenticated] = useState(
@@ -27,64 +27,69 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          {/* Home Page */}
-          <Route path="/" element={<LandingPage />} />
+      <UserProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            {/* Home Page */}
+            <Route path="/" element={<LandingPage />} />
 
-          {/* Login and signup routes */}
-          <Route path="/login" element={authenticated ? <Main /> : <Login />} />
-          <Route
-            path="/signup"
-            element={authenticated ? <Main /> : <SignUp />}
-          />
+            {/* Login and signup routes */}
+            <Route
+              path="/login"
+              element={authenticated ? <Main /> : <Login />}
+            />
+            <Route
+              path="/signup"
+              element={authenticated ? <Main /> : <SignUp />}
+            />
 
-          {/* Page Routes */}
-          <Route
-            path="/dashboard"
-            element={authenticated ? <Main /> : <Login />}
-          />
-          <Route
-            path="/programs"
-            element={
-              authenticated ? (
-                <ProgramProvider>
-                  <ProgramsPage userView={false}/>
-                </ProgramProvider>
-              ) : (
-                <ProgramProvider>
-                  <ProgramsPage userView={false}/>
-                </ProgramProvider>
-              )
-            }
-          />
-          <Route
-            path="/programs/log"
-            element={authenticated ? <Log /> : <Login />}
-          />
-          <Route
-            path="/profile/1"
-            element={authenticated ? <Profile /> : <Login />}
-          />
-          <Route
-            path="/chartworkout/4"
-            element={authenticated ? <ChartWorkout /> : <Login />}
-          />
-          <Route
-            path="/programs/:program_id/sessions/:session_id"
-            element={authenticated ? <SessionDetail /> : <Login />}
-          />
-          <Route
-            path="/programs/1/sessions/1/exercise/1"
-            element={authenticated ? <ExerciseLog /> : <Login />}
-          />
+            {/* Page Routes */}
+            <Route
+              path="/dashboard"
+              element={authenticated ? <Main /> : <Login />}
+            />
+            <Route
+              path="/programs"
+              element={
+                authenticated ? (
+                  <ProgramProvider>
+                    <ProgramsPage userView={false} />
+                  </ProgramProvider>
+                ) : (
+                  <ProgramProvider>
+                    <ProgramsPage userView={false} />
+                  </ProgramProvider>
+                )
+              }
+            />
+            <Route
+              path="/programs/log"
+              element={authenticated ? <Log /> : <Login />}
+            />
+            <Route
+              path="/profile/1"
+              element={authenticated ? <Profile /> : <Login />}
+            />
+            <Route
+              path="/chartworkout/4"
+              element={authenticated ? <ChartWorkout /> : <Login />}
+            />
+            <Route
+              path="/programs/:program_id/sessions/:session_id"
+              element={authenticated ? <SessionDetail /> : <Login />}
+            />
+            <Route
+              path="/programs/1/sessions/1/exercise/1"
+              element={authenticated ? <ExerciseLog /> : <Login />}
+            />
 
-          {/* Testing Routes */}
-          <Route path="/test" element={<DevTest />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+            {/* Testing Routes */}
+            <Route path="/test" element={<DevTest />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </UserProvider>
     </div>
   );
 }
--- client/src/components/Programs/CreateProgram.jsx
+++ client/src/components/Programs/CreateProgram.jsx
@@ -1,6 +1,6 @@
-import React from "react"
-import { useContext, useState }  from "react"
-import { programContext } from '../../providers/ProgramProvider';
+import React from "react";
+import { useContext, useState } from "react";
+import { programContext } from "../../contexts/ProgramProvider";
 
 const CreateProgram = () => {
   const { createProgram, setNewProgram } = useContext(programContext);
@@ -13,8 +13,6 @@ const CreateProgram = () => {
       [name]: value,
     }));
   };
-  
-  
 
   return (
     <div className="p-5">
@@ -22,17 +20,37 @@ const CreateProgram = () => {
         <div className="container bg-dark text-white rounded">
           <h3 className="text-warning fw-bold">Create Program</h3>
           <div className="text-start">
-            <label for="desciption" className="form-label">Name</label>
-            <input onChange={handleChange} className="form-control" id="name" name="name" placeholder="name" required />
+            <label for="desciption" className="form-label">
+              Name
+            </label>
+            <input
+              onChange={handleChange}
+              className="form-control"
+              id="name"
+              name="name"
+              placeholder="name"
+              required
+            />
           </div>
 
           <div className="text-start py-3">
-            <label for="description" class="form-label">Description</label>
-            <textarea onChange={handleChange} class="form-control" id="description" name="description" placeholder="description"required />
+            <label for="description" class="form-label">
+              Description
+            </label>
+            <textarea
+              onChange={handleChange}
+              class="form-control"
+              id="description"
+              name="description"
+              placeholder="description"
+              required
+            />
           </div>
 
           <div className="d-grid pt-3">
-            <button onClick={() => createProgram()} className="btn btn-warning">Create</button>
+            <button onClick={() => createProgram()} className="btn btn-warning">
+              Create
+            </button>
           </div>
         </div>
       </form>
