3fb8e72acb4480a6c9217910344878e1aec5048d
Phat Luong <44761311+luongtanphat25@users.noreply.github.com>
2023-08-08 21:47:23 -0400

    small fix

--- client/src/components/Sessions/SessionItem.jsx
+++ client/src/components/Sessions/SessionItem.jsx
@@ -1,67 +1,69 @@
-import React from "react";
-import { useState } from "react";
-import axios from "axios";
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 const SessionItem = (props) => {
   const [deleteMode, setDeleteMode] = useState();
+  const [editSet, setEditSet] = useState(false);
 
   const deleteItem = async () => {
-    for(const set of props.sets) {
-      if(set.exercise_name === props.exercise.name){
+    for (const set of props.sets) {
+      if (set.exercise_name === props.exercise.name) {
         try {
           // Submit form data to the server
           await axios.post(`/sets/${set.id}/delete`);
           // Update the profile state with the newly created/updated profile data
-          
         } catch (error) {
           console.error('Error creating session:', error);
         }
       }
     }
     window.location.reload();
-  }
+  };
+
+  const onEditSets = () => {};
 
   const setList = props.sets
-      .filter((set) => set.exercise_name === props.exercise.name)
-      .map((set) => (
-        <span className="badge text-bg-light" key={set.id}>
-          {set.resistant}lbs/{set.reps}
-        </span>
-      ));
+    .filter((set) => set.exercise_name === props.exercise.name)
+    .map((set) => (
+      <span className="badge text-bg-light" key={set.id}>
+        {set.resistant}lbs/{set.reps}
+      </span>
+    ));
   return (
     <tr>
-      {!deleteMode ? 
-      <td role="button" className="p-3 d-flex justify-content-between">
-        <div>
-          <h6 className="">{props.exercise.name}</h6>
-          <div className="d-flex gap-2 flex-wrap">{setList}</div>
-        </div>
-        {/* {add edit - delete button} */}
-        <div className="align-self-center">
-          <button className="btn">
-            <i className="fa-regular fa-pen-to-square fa-xl text-light"></i>
-          </button>
-          <button className="btn" onClick={() => setDeleteMode(true)}>
-            <i className="fa-regular fa-trash-can fa-xl text-danger"></i>
-          </button>
-        </div>
-      </td> 
-      :
-      <td role="button" className="p-3 d-flex justify-content-between">
-        <div>
-          <h6 className="">Deleting {props.exercise.name}</h6>
-        </div>
-        {/* {add edit - delete button} */}
-        <div className="align-self-center">
-          <button className="btn" onClick={() => deleteItem()}>
-            <i className="fa-solid fa-check fa-xl text-danger"></i>
-          </button>
-          <button className="btn" onClick={() => setDeleteMode(false)}>
-            <i className="fa-solid fa-x fa-xl text-light"></i>
-          </button>
-        </div>
-      </td> 
-      }
+      {!deleteMode ? (
+        <td role="button" className="p-3 d-flex justify-content-between">
+          <div>
+            <h6 className="">{props.exercise.name}</h6>
+            <div className="d-flex gap-2 flex-wrap">{setList}</div>
+          </div>
+          {/* {add edit - delete button} */}
+          <div className="align-self-center">
+            <button className="btn" onClick={onEditSets}>
+              <i className="fa-regular fa-pen-to-square fa-xl text-light"></i>
+            </button>
+            <button className="btn" onClick={() => setDeleteMode(true)}>
+              <i className="fa-regular fa-trash-can fa-xl text-danger"></i>
+            </button>
+          </div>
+        </td>
+      ) : (
+        <td role="button" className="p-3 d-flex justify-content-between">
+          <div>
+            <h6 className="">Deleting {props.exercise.name}</h6>
+          </div>
+          {/* {add edit - delete button} */}
+          <div className="align-self-center">
+            <button className="btn" onClick={() => deleteItem()}>
+              <i className="fa-solid fa-check fa-xl text-danger"></i>
+            </button>
+            <button className="btn" onClick={() => setDeleteMode(false)}>
+              <i className="fa-solid fa-x fa-xl text-light"></i>
+            </button>
+          </div>
+        </td>
+      )}
     </tr>
   );
 };
