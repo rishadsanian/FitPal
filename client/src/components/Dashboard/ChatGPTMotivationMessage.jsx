b88e780ffb9c1cd974b1253f0ad96fd16ad7022d
Rishad Alam <rishad.s.a@gmail.com>
2023-08-10 05:42:01 -0400

    chatgpt - update engine, set default message if errors, note token price

--- client/src/components/Dashboard/ChatGPTMotivationMessage.jsx
+++ client/src/components/Dashboard/ChatGPTMotivationMessage.jsx
@@ -7,6 +7,8 @@ const MotivationalMessage = () => {
   const [motivationalMessage, setMotivationalMessage] = useState("");
   const [lastGeneratedDate, setLastGeneratedDate] = useState(null);
 
+  //need program for user if they are enrolled in one
+
   useEffect(() => {
     // Fetch the last generated date from storage (localStorage, AsyncStorage, etc.)
     const storedDate = localStorage.getItem("lastGeneratedDate");
@@ -15,24 +17,28 @@ const MotivationalMessage = () => {
     }
 
     fetchMotivationalMessage();
-  }, []);
+  }, [profile]);
 
   const fetchMotivationalMessage = async () => {
     try {
       const currentDate = new Date();
-      if (!lastGeneratedDate || currentDate.toDateString() !== lastGeneratedDate.toDateString()) {
+      if (
+        !lastGeneratedDate ||
+        currentDate.toDateString() !== lastGeneratedDate.toDateString()
+      ) {
         // Update the last generated date
         localStorage.setItem("lastGeneratedDate", currentDate.toISOString());
         setLastGeneratedDate(currentDate);
 
         // Generate a new motivational message
-        const prompt = `Generate a motivational message for our fitnes app for a user who is a ${profile.fitness_level} with a goal of ${profile.goal}. They recently completed a workout that included ${recentWorkout.exercise_name} with ${recentWorkout.resistance} lbs of resistance and ${recentWorkout.reps} reps.`;
+        const prompt = `Generate a motivational message for our fitnes app for a user who is a ${profile.fitness_level} with a goal of ${profile.goal}. They recently completed a workout that included ${recentWorkout.exercise_name} with ${recentWorkout.resistance} lbs of resistance and ${recentWorkout.reps} reps.`; //to add session name,
 
         const response = await axios.post(
-          "https://api.openai.com/v1/engines/davinci-codex/completions",
+          // "https://api.openai.com/v1/engines/davinci-codex/completions",
+          "https://api.openai.com/v1/chat/completions",
           {
             prompt: prompt, //different prompts can be passed depending on use cases
-            max_tokens: 50, // need to adjust this for length of message
+            max_tokens: 100, // need to adjust this for length of message
             temperature: 0.7,
             api_key: "YOUR_OPENAI_API_KEY",
           }
@@ -46,11 +52,15 @@ const MotivationalMessage = () => {
       }
     } catch (error) {
       console.error("Error fetching motivational message:", error);
+      setMotivationalMessage(
+        "Unlock your potential with every move. Embrace the challenge, push your limits, and celebrate your progress. Your fitness journey is a testament to your strength and determination. Keep going!"
+      ); //54 tokens
     }
   };
 
   return (
     <div className="motivational-message">
+      <h2>Daily Summary</h2>
       <p>{motivationalMessage}</p>
     </div>
   );
