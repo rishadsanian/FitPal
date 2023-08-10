0e2eea74f3e6fd665b03298b20b3f7bf7ab7a84d
Rishad Alam <rishad.s.a@gmail.com>
2023-08-10 10:21:41 -0400

    implemented chat gpt summary, some bugs present with length of message and profile state

--- client/src/components/Dashboard/ChatGptDailySummary.jsx
+++ client/src/components/Dashboard/ChatGptDailySummary.jsx
@@ -5,7 +5,7 @@ import { useWorkoutContext } from "../../contexts/WorkoutContext";
 
 //Chat GPT motivation message Broiler Pate
 const ChatGptDailySummary = () => {
-  const { profile} = useProfileContext();
+  const { profile } = useProfileContext();
   const [motivationalMessage, setMotivationalMessage] = useState("");
   const [lastGeneratedDate, setLastGeneratedDate] = useState(null);
 
@@ -20,55 +20,14 @@ const ChatGptDailySummary = () => {
 
     fetchMotivationalMessage();
   }, [profile]);
-
-  // const fetchMotivationalMessage = async () => {
-  //   try {
-  //     const currentDate = new Date();
-  //     if (
-  //       !lastGeneratedDate ||
-  //       currentDate.toDateString() !== lastGeneratedDate.toDateString()
-  //     ) {
-  //       // Update the last generated date
-  //       localStorage.setItem("lastGeneratedDate", currentDate.toISOString());
-  //       setLastGeneratedDate(currentDate);
-
-  //       // Generate a new motivational message
-  //       const prompt = `Generate a motivational message for our fitnes app for a user who is a ${profile.fitness_level} with a goal of ${profile.goal}. Recommend exercises for the day and a weekly program`; //to add session name, list of exerciss.
-  //       // const prompt = `Generate a motivational message for our fitnes app for a user who is a ${profile.fitness_level} with a goal of ${profile.goal}. They recently completed a workout that included ${recentWorkout.exercise_name} with ${recentWorkout.resistance} lbs of resistance and ${recentWorkout.reps} reps.`; //to add session name, list of exerciss.
-
-  //       const response = await axios.post(
-  //         // "https://api.openai.com/v1/engines/davinci-codex/completions",
-  //         "https://api.openai.com/v1/chat/completions",
-  //         {
-  //           prompt: prompt, //different prompts can be passed depending on use cases
-  //           max_tokens: 100, // need to adjust this for length of message
-  //           temperature: 0.7,
-  //           api_key: process.env.YOUR_OPENAI_API_KEY, //need .env and .env example to be setup
-  //         }
-  //       );
-
-  //       setMotivationalMessage(response.data.choices[0].text);
-  //     } else {
-  //       // Use the cached motivational message
-  //       const cachedMessage = localStorage.getItem("cachedMessage");
-  //       setMotivationalMessage(cachedMessage);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error fetching motivational message:", error);
-  //     setMotivationalMessage(
-  //       "Unlock your potential with every move. Embrace the challenge, push your limits, and celebrate your progress. Your fitness journey is a testament to your strength and determination. Keep going!"
-  //     ); //54 tokens
-  //   }
-  // };
-
+  
   const fetchMotivationalMessage = async () => {
     try {
       const currentDate = new Date();
       if (
         !lastGeneratedDate ||
         currentDate.toDateString() !== lastGeneratedDate.toDateString()
-      ) 
-      {
+      ) {
         // Update the last generated date
         localStorage.setItem("lastGeneratedDate", currentDate.toISOString());
         setLastGeneratedDate(currentDate);
@@ -76,22 +35,27 @@ const ChatGptDailySummary = () => {
         // Generate a new motivational message
         const prompt = `Generate a motivational message for our fitness app for a user who is a ${profile.fitness_level} with a goal of ${profile.goal}. Recommend exercises for the day and a weekly program`;
 
+        const key = process.env.REACT_APP_YOUR_OPENAI_API_KEY;
+        // const key = "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";
+        console.log("open ai key:", key);
+
         const response = await axios.post(
-          "https://api.openai.com/v1/engines/davinci-codex/completions",
+          "https://api.openai.com/v1/chat/completions",
           {
-            prompt: prompt,
             max_tokens: 100,
             temperature: 0.7,
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: prompt }],
           },
           {
             headers: {
               "Content-Type": "application/json",
-              Authorization: `Bearer ${process.env.YOUR_OPENAI_API_KEY}`,
+              Authorization: `Bearer ${key}`,
             },
           }
         );
-
-        setMotivationalMessage(response.data.choices[0].text);
+        console.log("Response data", response.data);
+        setMotivationalMessage(response.data.choices[0].message.content);
       } else {
         // Use the cached motivational message
         const cachedMessage = localStorage.getItem("cachedMessage");
@@ -108,7 +72,7 @@ const ChatGptDailySummary = () => {
   return (
     <div className="motivational-message">
       <h2>Daily Summary</h2>
-      <p>{motivationalMessage}</p>
+      <p className="text-white">{motivationalMessage}</p>
     </div>
   );
 };
