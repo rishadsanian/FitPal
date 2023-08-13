d391f05c669a4e40bb9d93c94b3172ac93001d4d
Rishad Alam <rishad.s.a@gmail.com>
2023-08-13 13:28:33 -0400

    clean up profile

--- client/src/components/Dashboard/Profile.jsx
+++ client/src/components/Dashboard/Profile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import '../../styles/Profile.css';
 import { useProfileContext } from '../../contexts/ProfileContext';
