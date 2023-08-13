72c1a9ed5aae38601a90e1fb1e30834ac000d592
Rishad Alam <rishad.s.a@gmail.com>
2023-08-13 15:12:04 -0400

    add focusring effect on badge on weight chart

--- client/src/components/Dashboard/WeightTracker.jsx
+++ client/src/components/Dashboard/WeightTracker.jsx
@@ -2,32 +2,34 @@ import React, { useContext } from "react";
 import WeightChart from "./ChartWeight";
 import { useProfileContext } from "../../contexts/ProfileContext";
 import { userContext } from "../../contexts/UserContext";
+import '../../styles/WeightTracker.css';
+
+
 
 const WeightTracker = () => {
   const { selectedInterval, setSelectedInterval } = useProfileContext();
   const { userId } = useContext(userContext);
 
   return (
-    <div className="card bg-dark weekly-tracker-card mb-3 p-3 border border-secondary rounded border-3 ">
-      
-      <h3 className="pt-4 pb-2 text-warning fw-bold weekly-tracker-header py-5 opacity-75">Weight Tracker</h3>
+    <div className="card bg-dark weight-tracker-card mb-3 p-3 border border-secondary rounded border-3 ">
+            <h3 className="pt-4 pb-2 text-warning fw-bold weight-tracker-header py-5 opacity-75 ">Weight Tracker</h3>
       <div className="pt-3 pb-3">
         <span
-          className="badge bg-dark me-2 pt-1 opacity-75 text-warning"
+          className="interval badge me-2 focus-ring pt-1 opacity-75 focus-ring focus-ring-warning py-1 px-2 text-decoration-none"
           style={{ cursor: "pointer" }}
           onClick={() => setSelectedInterval("7d")}
         >
           7 Days
         </span>
         <span
-          className="badge bg-dark me-2 pt-1 opacity-75 text-warning"
+          className="interval badge me-2 focus-ring pt-1 opacity-75 focus-ring focus-ring-warning py-1 px-2 text-decoration-none"
           style={{ cursor: "pointer" }}
           onClick={() => setSelectedInterval("30d")}
         >
           30 Days
         </span>
         <span
-          className="badge bg-dark me-2 pt-1 opacity-75 text-warning"
+          className="interval badge me-2 focus-ring pt-1 opacity-75 focus-ring focus-ring-warning py-1 px-2 text-decoration-none"
           style={{ cursor: "pointer" }}
           onClick={() => setSelectedInterval("1yr")}
         >
