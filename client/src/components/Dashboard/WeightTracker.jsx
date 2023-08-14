b244a00b1e2ec6bec2aee436bf9428b3cf8a62fb
Rishad Alam <rishad.s.a@gmail.com>
2023-08-14 04:46:29 -0400

    weightchart - add styling to show interval clicks

--- client/src/components/Dashboard/WeightTracker.jsx
+++ client/src/components/Dashboard/WeightTracker.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import WeightChart from "./ChartWeight";
 import { useProfileContext } from "../../contexts/ProfileContext";
 import { userContext } from "../../contexts/UserContext";
@@ -7,6 +7,13 @@ import "../../styles/WeightTracker.css";
 const WeightTracker = () => {
   const { selectedInterval, setSelectedInterval } = useProfileContext();
   const { userId } = useContext(userContext);
+  const [selectedTab, setSelectedTab] = useState("7d"); // Add this state
+
+  const handleIntervalClick = (interval) => {
+    setSelectedInterval(interval);
+    setSelectedTab(interval); // Update the selected tab
+  };
+
 
   return (
     <div className="card bg-dark weight-tracker-card mb-3 p-3 border border-secondary rounded border-3 ">
@@ -16,23 +23,35 @@ const WeightTracker = () => {
 
       <div className="pt-3 pb-3">
         <span
-          className="interval badge me-2 focus-ring pt-1 opacity-75 focus-ring focus-ring-warning py-1 px-2 text-decoration-none"
-          style={{ cursor: "pointer" }}
-          onClick={() => setSelectedInterval("7d")}
+          className={`interval badge me-2 focus-ring pt-1 opacity-75 ${
+            selectedTab === "7d" ? "focus-ring-warning" : ""
+          }`}
+          style={{ cursor: "pointer",
+          color: selectedTab === "7d"? "#343a40" : "" ,
+          backgroundColor: selectedTab === "7d" ? "#ffc107" : "", }}
+          onClick={() => handleIntervalClick("7d")}
         >
           7 Days
         </span>
         <span
-          className="interval badge me-2 focus-ring pt-1 opacity-75 focus-ring focus-ring-warning py-1 px-2 text-decoration-none"
-          style={{ cursor: "pointer" }}
-          onClick={() => setSelectedInterval("30d")}
+          className={`interval badge me-2 focus-ring pt-1 opacity-75 ${
+            selectedTab === "30d" ? "focus-ring-warning" : ""
+          }`}
+          style={{ cursor: "pointer",
+          color: selectedTab === "30d"? "#343a40" : "" ,
+          backgroundColor: selectedTab === "30d" ? "#ffc107" : "", }}
+          onClick={() => handleIntervalClick("30d")}
         >
           30 Days
         </span>
         <span
-          className="interval badge me-2 focus-ring pt-1 opacity-75 focus-ring focus-ring-warning py-1 px-2 text-decoration-none"
-          style={{ cursor: "pointer" }}
-          onClick={() => setSelectedInterval("1yr")}
+          className={`interval badge me-2 focus-ring pt-1 opacity-75 ${
+            selectedTab === "1yr" ? "focus-ring-warning" : ""
+          }`}
+          style={{ cursor: "pointer",
+          color: selectedTab === "1yr"? "#343a40" : "" ,
+          backgroundColor: selectedTab === "1yr" ? "#ffc107" : "", }}
+          onClick={() => handleIntervalClick("1yr")}
         >
           1 Year
         </span>
