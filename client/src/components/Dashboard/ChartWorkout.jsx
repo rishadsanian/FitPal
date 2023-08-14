c0a48ff07fcfb0c6197f12a3f2dabca6223dcfcd
Rishad Alam <rishad.s.a@gmail.com>
2023-08-14 03:18:45 -0400

    workout-chart - uupdate to fix bugs and conditional rendering

--- client/src/components/Dashboard/ChartWorkout.jsx
+++ client/src/components/Dashboard/ChartWorkout.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useRef, useContext } from "react";
 import axios from "axios";
 import {
@@ -19,52 +18,63 @@ const ChartWorkout = () => {
   const { workoutHistory } = useWorkoutContext();
   //state
   const [workoutData, setWorkoutData] = useState([]);
+  const [workoutsThisWeek, SetWorkoutsThisWeek] = useState([]);
   //useref needed to fix canvas clash bug
   const chartRef = useRef(null);
 
-  // get data from db
+  // Fetch workout data from db
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`/api/chartworkout/${userId}`); //hard-coded change to current user
+        const response = await axios.get(`/api/chartworkout/${userId}`);
         setWorkoutData(response.data);
       } catch (error) {
         console.error("Error fetching workout data:", error);
       }
     };
     fetchData();
-  }, [workoutHistory]);
+  }, [userId, workoutData, workoutHistory]); // Only fetch data when userId changes
 
-  //extract and process needed data
-  const processWorkoutData = () => {
+  // Update workoutsThisWeek whenever workoutData/history changes
+  useEffect(() => {
     const currentDate = moment();
-    //Process data for one week
     const startDate = moment(currentDate).startOf("isoWeek");
     const endDate = moment(currentDate).endOf("isoWeek");
-    //Filter log data for one week
-    const workoutsThisWeek = workoutData.filter((workout) => {
-      const workoutDate = moment(workout.timestamp);
-      return workoutDate.isBetween(startDate, endDate, null, "[]");
-    });
-    //
-    const exercisesPerDay = new Array(7).fill(0);
+    SetWorkoutsThisWeek(
+      workoutData.filter((workout) => {
+        const workoutDate = moment(workout.timestamp);
+        return workoutDate.isBetween(startDate, endDate, null, "[]");
+      })
+    );
+  }, [workoutData, workoutHistory]);
 
+  // Update the chart whenever workoutData or workoutsThisWeek changes
+  useEffect(() => {
+    if (chartRef.current) {
+      chartRef.current.data.datasets[0].data = processWorkoutData();
+      chartRef.current.update();
+    }
+  }, [workoutData, workoutsThisWeek, workoutHistory]);
+
+  // Process workout data
+  const processWorkoutData = () => {
+    const exercisesPerDay = new Array(7).fill(0);
     workoutsThisWeek.forEach((workout) => {
       const workoutDate = moment(workout.timestamp);
       const dayIndex = workoutDate.isoWeekday() - 1; // 0 for Monday, 1 for Tuesday, ..., 6 for Sunday
       exercisesPerDay[dayIndex]++;
     });
-
     return exercisesPerDay;
   };
 
+  // Initialize or update the chart
   useEffect(() => {
-    // Initialize or update the chart when the workoutData changes
-    if (!chartRef.current) {
-      // Create chart for the first time
+    if (chartRef.current) {
+      chartRef.current.data.datasets[0].data = processWorkoutData();
+      chartRef.current.update();
+    } else {
       const ctx = document.getElementById("workoutChart").getContext("2d");
       ctx.canvas.height = 400;
-      //Configurations for chart
       const chartConfig = {
         type: "bar",
         data: {
@@ -85,23 +95,14 @@ const ChartWorkout = () => {
           scales: {
             y: {
               beginAtZero: true,
-              stepSize: 1, // Set the step size to 1 to show whole numbers on the y-axis
+              stepSize: 1,
             },
           },
         },
       };
       chartRef.current = new Chart(ctx, chartConfig);
-    } else {
-      // Update the existing chart
-      chartRef.current.data.datasets[0].data = processWorkoutData();
-      chartRef.current.update();
     }
-  }, [workoutData.length]);
-
-  useEffect(() => {
-    chartRef.current.data.datasets[0].data = processWorkoutData();
-    chartRef.current.update();
-  }, [workoutData.length]);
+  }, [workoutData, workoutsThisWeek, workoutHistory]);
 
   const currentDate = moment();
   const startDate = moment(currentDate)
@@ -121,21 +122,21 @@ const ChartWorkout = () => {
           </p>
           <div
             className="chart-wrapper"
-            style={{ display: workoutData.length > 0 ? "block" : "none" }}
+            style={{ display: workoutsThisWeek.length > 0 ? "block" : "none" }}
           >
             <div className="chart-container">
               <canvas id="workoutChart" height="100%" width="100%" />
             </div>
           </div>
-
           <span className="d-flex align-items-center justify-content-center pt-4 pb-0">
             <a href="#log" className="text-decoration-none text-warning">
               <i className="circle-add-icon fas fa-plus-circle fa-3x opacity-75"></i>
             </a>
           </span>
-          {/* {workoutData.length === 0 && ( */}
-          {workoutData.length === 0 && (
-            <p className="text-secondary pt-3">No workouts recorded for this week</p>
+          {workoutsThisWeek.length === 0 && (
+            <p className="text-secondary pt-3">
+              No workouts recorded for this week
+            </p>
           )}
         </div>
       </div>
